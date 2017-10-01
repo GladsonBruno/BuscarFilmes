1bca77861c791e9474d9e47adedd127697ded02f
Gladson <gladsonbruno16@gmail.com>
2017-10-01 20:59:22 -0300

    Intent para pegar o nome do filme e passar para a proxima ação para fazer o tratamento do resultado com a api

--- app/src/main/java/com/example/gladson/buscafilmes/MainActivity.java
+++ app/src/main/java/com/example/gladson/buscafilmes/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.gladson.buscafilmes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String FILME = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void pesquisarFilme(View view){
+        Intent intent = new Intent(this,resultadoPesquisaActivity.class);
+        EditText editText = (EditText) findViewById(R.id.entrada);
+        String filme = editText.getText().toString();
+        intent.putExtra(FILME, filme);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/gladson/buscafilmes/resultadoPesquisaActivity.java
@@ -0,0 +1,13 @@
+package com.example.gladson.buscafilmes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class resultadoPesquisaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado_pesquisa);
+    }
+}
