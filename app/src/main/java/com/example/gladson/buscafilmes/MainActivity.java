4c138b4591dbff4bc5961e6ce95290f22a199f71
Gladson <gladsonbruno16@gmail.com>
2017-10-01 21:07:54 -0300

    Intent com o nome do filme salvo para ser usado na proxima tela. Tela de resultado instanciada

--- app/src/main/java/com/example/gladson/buscafilmes/MainActivity.java
+++ app/src/main/java/com/example/gladson/buscafilmes/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    //Guarda o nome do filme
     public final static String FILME = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +16,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void pesquisarFilme(View view){
+        //Cria um vinculo entre a primeira e a segunda pagina
         Intent intent = new Intent(this,resultadoPesquisaActivity.class);
+        //Pega a editText da primeira tela
         EditText editText = (EditText) findViewById(R.id.entrada);
+        //atribui a variavel filme o nomme digitado no EditText component
         String filme = editText.getText().toString();
+        //Adiciona o valor e editText ao intent
         intent.putExtra(FILME, filme);
+        //Instancia a proxima tela
         startActivity(intent);
     }
 }
