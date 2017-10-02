838a28dbaa1cdde35a3e35c531cbf2ae103b271f
Gladson <gladsonbruno16@gmail.com>
2017-10-02 17:06:05 -0300

    Estudando e montando a estrutura da Requisição Volley

--- app/src/main/java/com/example/gladson/buscafilmes/MainActivity.java
+++ app/src/main/java/com/example/gladson/buscafilmes/MainActivity.java
@@ -5,6 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
     //Guarda o nome do filme
@@ -13,6 +21,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView teste = (TextView) findViewById(R.id.testeVolley);
+
+        //Inicializando a requisição
+        RequestQueue request = Volley.newRequestQueue(this);
+        String url = "http://www.omdbapi.com/?i=tt3896198&apikey=ea23cea2";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response){
+                        teste.setText("Resultado:" + response.toString());
+                    }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+                teste.setText(error.getMessage());
+            }
+        });
+        request.add(stringRequest);
     }
 
     /*
