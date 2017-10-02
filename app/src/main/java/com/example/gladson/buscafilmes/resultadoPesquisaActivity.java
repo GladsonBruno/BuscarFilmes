ac3d9951e75379659de1f64f92c2dc73eb9e926e
Gladson <gladsonbruno16@gmail.com>
2017-10-01 21:27:07 -0300

    Tentando montar a requisição

--- app/src/main/java/com/example/gladson/buscafilmes/resultadoPesquisaActivity.java
+++ app/src/main/java/com/example/gladson/buscafilmes/resultadoPesquisaActivity.java
@@ -1,10 +1,13 @@
 package com.example.gladson.buscafilmes;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.net.URL;
 
 public class resultadoPesquisaActivity extends AppCompatActivity {
 
@@ -12,6 +15,10 @@ public class resultadoPesquisaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado_pesquisa);
+
+
+
+
         //Captura o valor da intent
         Intent intent = getIntent();
         //Atribui o valor da intent a uma variavel
@@ -24,5 +31,15 @@ public class resultadoPesquisaActivity extends AppCompatActivity {
         //Adiciona o elemento a um layout
         ViewGroup layout = (ViewGroup) findViewById(R.id.mostrarResultado);
         layout.addView(textView);
+    }
+
+    public void PesquisarFilme(final String titulo){
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                String url = "http://www.omdbapi.com/?t=" + titulo.toString();
+                
+            }
+        });
     }
 }
