c3e73bb13442300db5e4d917eea5c61b0b39bacb
Gladson <gladsonbruno16@gmail.com>
2017-10-02 21:13:06 -0300

    Concluido - Faltar formatar a tela para ficar melhor Removi métodos desnecessários e coloquei a requisição na página final

--- app/src/main/java/com/example/gladson/buscafilmes/MainActivity.java
+++ app/src/main/java/com/example/gladson/buscafilmes/MainActivity.java
@@ -5,68 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    //Guarda o nome do filme
+    //Variável que Guarda o nome do filme para passar para o intent
     public final static String FILME = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
 
-    public void pesquisarFilme(View view){
-        EditText editText = (EditText) findViewById(R.id.entrada);
-        requisitarURL(editText.getText().toString());
-    }
-    public void requisitarURL(String filme){
-        //Cria um vinculo entre a primeira e a segunda pagina
-        Intent intent = new Intent(this,resultadoPesquisaActivity.class);
-        //Pega a editText da primeira tela
-
-        final TextView guardarResposta = (TextView) findViewById(R.id.testeVolley);
-
-        //Inicializando a requisição
-        RequestQueue request = Volley.newRequestQueue(this);
-        String url = "http://www.omdbapi.com/?apikey=ea23cea2&t=" + filme;
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>(){
-                    @Override
-                    public void onResponse(JSONObject response){
-                        try{
-                            guardarResposta.setText(response.getString("Title"));
-                        }catch (JSONException EX){
-                            guardarResposta.setText(EX.getMessage());
-                        }
-
-                    }
-                }, new Response.ErrorListener(){
-            @Override
-            public void onErrorResponse(VolleyError error){
-                guardarResposta.setText(error.getMessage());
-            }
-        });
-        request.add(jsonObjectRequest);
-        TextView resposta = (TextView) findViewById(R.id.testeVolley);
-        intent.putExtra(FILME, resposta.getText().toString());
-        //Instancia a proxima tela
+    public void pesquisarFilme(View view) {
+        Intent intent = new Intent(this, resultadoPesquisaActivity.class);
+        EditText valorDigitado = (EditText) findViewById(R.id.entrada);
+        intent.putExtra(FILME, valorDigitado.getText().toString());
         startActivity(intent);
-
     }
-
 }
--- app/src/main/java/com/example/gladson/buscafilmes/resultadoPesquisaActivity.java
+++ app/src/main/java/com/example/gladson/buscafilmes/resultadoPesquisaActivity.java
@@ -7,13 +7,15 @@ import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
-import javax.net.ssl.HttpsURLConnection;
-
-import static android.R.attr.start;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class resultadoPesquisaActivity extends AppCompatActivity {
 
@@ -25,12 +27,55 @@ public class resultadoPesquisaActivity extends AppCompatActivity {
         Intent intent = getIntent();
         //Atribui o valor da intent a uma variavel
         final String nomeFilmePesquisa = intent.getStringExtra(MainActivity.FILME);
+        //Quando tiver o caractere espaço subtitui pelo sinal +
+        String resultadoFormatado = nomeFilmePesquisa.replace(" ", "+");
         //Cria um elemento para mostra o resultado
         final TextView textView = new TextView(this);
         textView.setTextSize(40);
-        textView.setText(nomeFilmePesquisa);
+
+        //Inicializando a requisição
+        RequestQueue request = Volley.newRequestQueue(this);
+        //String formando a URL de pesquisa
+        String url = "http://www.omdbapi.com/?apikey=ea23cea2&t=" + resultadoFormatado;
+
+        //Requisição Volley do tipo JsonObject
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>(){
+                    @Override
+                    public void onResponse(JSONObject response){
+                        //Tratamento para manipular o resultado
+                        try{
+                            //Pego o resultado e pego o valor da chave especificado em getString
+                            String resposta = "Titulo: " + response.getString("Title");
+                            //Montando resposta com as informações obitidas
+                            resposta = resposta + "\nAno: " + response.get("Year");
+                            resposta = resposta + "\nLançamento: " + response.getString("Released");
+                            resposta = resposta + "\nRank: " + response.getString("Rated");
+                            resposta = resposta + "\nGênero: " + response.getString("Genre");
+                            resposta = resposta + "\nDiretor: " + response.getString("Director");
+                            resposta = resposta + "\nEscritor: " + response.getString("Writer");
+                            resposta = resposta + "\nAtores: " + response.getString("Actors");
+                            resposta = resposta + "\nLinguagem: " + response.getString("Language");
+                            resposta = resposta + "\nPaís de Orirgem: " + response.getString("Country");
 
 
+                            //Atribui o resultado para o TextView
+                            textView.setText(resposta);
+
+                        }catch (JSONException EX){
+                            //Tratamento de erros caso não consiga achar o objeto especificado na hora pegar o objeto
+                            textView.setText(EX.getMessage());
+                        }
+
+                    }
+                }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+                //Tratamento de erros caso não encontre resposta
+                textView.setText(error.getMessage());
+            }
+        });
+        request.add(jsonObjectRequest);
         //Adiciona o elemento a um layout
         ViewGroup layout = (ViewGroup) findViewById(R.id.mostrarResultado);
         layout.addView(textView);
